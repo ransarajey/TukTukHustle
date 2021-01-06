e77d3cb6ca58e0bf0c87c416c8e74dd2fbfaa011
Ransara Wijayasundara <ransarajey@gmail.com>
2021-01-06 18:28:29 +0530

    Added instruction

--- HomeScreen.cpp
+++ HomeScreen.cpp
@@ -4,6 +4,7 @@
 #include <Game.h>
 
 #include <QImage>
+#include <QMessageBox>
 
 Game * game;
 
@@ -33,3 +34,13 @@ void HomeScreen::on_StartGame_clicked()
     game->show();
 }
 
+
+void HomeScreen::on_StartGame_2_clicked()
+{
+    QString aboutText;
+    aboutText = "01: Use WASD keys to avoid collision with the ongoing traffic. \n";
+    aboutText += "02: Use horns(space) to move the traffic from your way.";
+
+    QMessageBox::about(this,"How to play?", aboutText);
+
+}
